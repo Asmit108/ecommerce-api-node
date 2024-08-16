b995a0cdc65125f70d98dcba75edd7795260e1e6
Asmit Kumar <69111936+Asmit108@users.noreply.github.com>
2024-08-16 17:33:48 +0530

    docker changes

--- src/config/db.js
+++ src/config/db.js
@@ -1,5 +1,6 @@
 const mongoose=require('mongoose')
-const mongodburl="mongodb+srv://asmitmbi108:ramanujam@cluster0.ofyttj3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
+const mongodburl=`mongodb+srv://asmitmbi108:${password}@cluster0.ofyttj3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 const connectDb=()=>{
     return mongoose.connect(mongodburl)
