8f1e384a58f9b012dd9fea9c90c934e7e4eff849
Asmit Kumar <69111936+Asmit108@users.noreply.github.com>
2024-08-16 17:50:34 +0530

    formatting

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,9 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose')
 const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
-const mongodburl=`mongodb+srv://asmitmbi108:${password}@cluster0.ofyttj3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const mongodburl = `mongodb+srv://asmitmbi108:${password}@cluster0.ofyttj3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
-const connectDb=()=>{
+const connectDb = () => {
     return mongoose.connect(mongodburl)
 }
 
-module.exports={connectDb}
+module.exports = { connectDb }
