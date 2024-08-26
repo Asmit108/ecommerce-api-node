1c8aef25eeed5629f4076ad0899f0ff2d4d9f643
Asmit Kumar <69111936+Asmit108@users.noreply.github.com>
2024-08-26 13:27:17 +0530

    key_id updated

--- src/config/razorpayClient.js
+++ src/config/razorpayClient.js
@@ -1,7 +1,7 @@
 const Razorpay = require('razorpay');
 
-const KEY_ID='rzp_test_hvf64RyhdJoDbt'
-const KEY_SECRET='U11VpzHg7tDWTXh9tROp2eWd'
+const KEY_ID='rzp_test_1nPr3ugBxoC3YI'
+const KEY_SECRET='8UwjIDyi1Y1OHh5AhMIxxBso'
 const razorpay = new Razorpay({
   key_id: KEY_ID,
   key_secret: KEY_SECRET
