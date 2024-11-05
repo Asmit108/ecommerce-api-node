2908c510433f870671344c4dffc01e0915a5cdea
Asmit Kumar <69111936+Asmit108@users.noreply.github.com>
2024-11-05 22:04:52 +0530

    port changed

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require(".");
 const { connectDb } = require("./config/db");
-const PORT=5454;
+const PORT=4000;
 app.listen(PORT,async()=>{
     await connectDb();
     console.log('ecommerce api listening on port:',PORT)
